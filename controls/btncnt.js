2e94d0d074621c079d7458921e0df4857cf5cc10
hemantsirsat <36109613+hemantsirsat@users.noreply.github.com>
2019-08-07 20:00:23 +0530

    Add files via upload

--- controls/btncnt.js
+++ controls/btncnt.js
@@ -18,7 +18,7 @@
 
 	document.getElementById('moveleft').addEventListener('touchstart',()=>{
 		var left1 = left + 1;
-		console.log(left1);
+		//console.log(left1);
 
 			var totalleft = firebase.database().ref('total left');
 
@@ -34,129 +34,100 @@
 			        var total = keyReq.val().left; 
 			        var t = parseInt(total); 
 			        t1 += t; 
-			        console.log(t1);
+			        console.log("left = "+t1);
 			    });
 			}
+		document.getElementById('moveleft').addEventListener('touchend',()=>{
+			totalleft.remove();	
+		});
 	});
-	document.getElementById('moveleft').addEventListener('touchend',()=>{
-		left1 = 0;
-		//console.log(left1);
 
-			var totalleft = firebase.database().ref('total left');
-
-			/*totalleft.push({
-				'left':left1*/
-				totalleft.child(left).remove();
-			
-	});
-	/////////////////////////////////////////////
+	
 	document.getElementById('moveright').addEventListener('touchstart',()=>{
 		var right1 =right + 1;
-		console.log(right1);
+		//console.log(right1);
 
 			var totalright = firebase.database().ref('total right');
 
 			totalright.push({
 				'right':right1
 			});
-	});
-	document.getElementById('moveright').addEventListener('touchend',()=>{
-		right1 = 0;
-		console.log(right1);
-
-			var totalright = firebase.database().ref('total right');
-
-			totalright.push({
-				'right':right1
+			var rootRef1 = firebase.database().ref(); 
+			var rootreq1 = rootRef1.child("total right"); 
+			rootreq1.on('value',getReq1); 
+			function getReq1(snap){ 
+			    var t11 = 0;
+			    snap.forEach(keyReq => { 
+			        var total1 = keyReq.val().right; 
+			        var t0 = parseInt(total1); 
+			        t11 += t0; 
+			        console.log("right = "+t11);
+			    });
+			}
+			document.getElementById('moveright').addEventListener('touchend',()=>{
+				totalright.remove();
 			});
 	});
-	///////////////////////////////////////////////
+
+
+
 	document.getElementById('moveup').addEventListener('touchstart',()=>{
 		var up1 = up + 1;
-		console.log(up1);
+		//console.log(up1);
 
 			var totalup = firebase.database().ref('total up');
 
 			totalup.push({
 				'up':up1
 			});
-	});
-	document.getElementById('moveup').addEventListener('touchend',()=>{
-		up1 = 0;
-		console.log(up1);
-
-			var totalup = firebase.database().ref('total up');
-
-			totalup.push({
-				'up':up1
-			});
+			var rootRef2 = firebase.database().ref();
+			var rootreq2 = rootRef2.child("total up"); 
+			rootreq2.on('value',getReq2); 
+			function getReq2(snap){ 
+			    var t2 = 0;
+			    snap.forEach(keyReq => { 
+			        var total2 = keyReq.val().up; 
+			        var t12 = parseInt(total2); 
+			        t2 += t12; 
+			        console.log("up =" +t2);
+			    });
+			}
+		document.getElementById('moveup').addEventListener('touchend',()=>{ 
+			totalup.remove();
+		});
 	});
 	////////////////////////////////////////////////
 	document.getElementById('movedown').addEventListener('touchstart',()=>{
 		var down1 = down + 1;
-		console.log(down1);
+		//console.log(down1);
 
 			var totaldown = firebase.database().ref('total down');
 
 			totaldown.push({
 				'down':down1
 			});
+			var rootRef3 = firebase.database().ref(); 
+			var rootreq3 = rootRef3.child("total down"); 
+			rootreq3.on('value',getReq3); 
+			function getReq3(snap){ 
+			    var t3 = 0;
+			    snap.forEach(keyReq => { 
+			        var total3 = keyReq.val().down; 
+			        var t13 = parseInt(total3); 
+			        t3 += t13; 
+			        console.log("down = "+t3);
+			    });
+			}
+		document.getElementById('movedown').addEventListener('touchend',()=>{
+			totaldown.remove();
+		});
 	});
-	document.getElementById('movedown').addEventListener('touchend',()=>{
-		down1 = 0;
-		console.log(down1);
-			var totaldown = firebase.database().ref('total down');
-
-			totaldown.push({
-				'down':down1
-			});
-	});
-/*var left_count = 0;
-var total_left = firebase.database().ref('total left');
-total_left.on('value',function(snapshot){
-	snapshot.forEach(function(childSnapshot){
-		left_count =  childSnapshot.val();
-		console.log(left_count);
-	});
-});
-*/
 
 
-var rootRef1 = firebase.database().ref(); 
-var rootreq1 = rootRef1.child("total right"); 
-rootreq1.on('value',getReq1); 
-function getReq1(snap){ 
-    var t11 = 0;
-    snap.forEach(keyReq => { 
-        var total1 = keyReq.val().right; 
-        var t0 = parseInt(total1); 
-        t11 += t0; 
-        console.log(t11);
-    });
-}
 
-var rootRef2 = firebase.database().ref(); 
-var rootreq2 = rootRef2.child("total up"); 
-rootreq2.on('value',getReq2); 
-function getReq2(snap){ 
-    var t2 = 0;
-    snap.forEach(keyReq => { 
-        var total2 = keyReq.val().up; 
-        var t12 = parseInt(total2); 
-        t2 += t12; 
-        console.log(t2);
-    });
-} 
 
-var rootRef3 = firebase.database().ref(); 
-var rootreq3 = rootRef3.child("total down"); 
-rootreq3.on('value',getReq3); 
-function getReq3(snap){ 
-    var t3 = 0;
-    snap.forEach(keyReq => { 
-        var total3 = keyReq.val().down; 
-        var t13 = parseInt(total3); 
-        t3 += t13; 
-        console.log(t3);
-    });
-}   
+
+
+ 
+
+   
